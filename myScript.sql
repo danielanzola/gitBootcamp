4288082391b5699abfa8cbe9e37aecbe6191d382
Daniel Anzola <danielanzola@usf.edu>
2021-03-26 13:15:07 -0400

    Added rm table

--- myScript.sql
+++ myScript.sql
@@ -7,3 +7,22 @@ REPLACE INTO `appiandb`.`GID_REF_reference` (`refId`, `refType`, `refValue`, `is
 REPLACE INTO `appiandb`.`GID_REF_reference` (`refId`, `refType`, `refValue`, `isActive`, `createdOn`, `createdBy`) VALUES ('107', 'affiliation', 'ROTC', '1', UTC_TIMESTAMP(), 'U69227703');
 REPLACE INTO `appiandb`.`GID_REF_reference` (`refId`, `refType`, `refValue`, `isActive`, `createdOn`, `createdBy`) VALUES ('108', 'affiliation', 'INTO', '1', UTC_TIMESTAMP(), 'U69227703');
 REPLACE INTO `appiandb`.`GID_REF_reference` (`refId`, `refType`, `refValue`, `isActive`, `createdOn`, `createdBy`) VALUES ('109', 'affiliation', 'Research-Related', '1', UTC_TIMESTAMP(), 'U69227703');
+
+-- Create Relationship Mapping table
+CREATE TABLE IF NOT EXISTS `appiandb`.`GID_relationshipMapping` (
+  `relationshipMappingId` INT(11) NOT NULL AUTO_INCREMENT,
+  `affiliation` INT(11) NULL,
+  `collegeCode` VARCHAR(9) NULL,
+  `globalDeptId` INT(11) NULL,
+  `globalDeptDesc` VARCHAR(45) NULL,
+  `isActive` TINYINT(1) NULL,
+  `createdOn` DATETIME NULL,
+  `createdBy` VARCHAR(9) NULL,
+  `updatedOn` DATETIME NULL,
+  `updatedBy` VARCHAR(9) NULL,
+  PRIMARY KEY (`relationshipMappingId`),
+  CONSTRAINT  `FK_GID_relationshipMapping_affiliation`
+  FOREIGN KEY (`affiliation`)
+  REFERENCES `appiandb`.`GID_REF_reference` (`refId`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION);
