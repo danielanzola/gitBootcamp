0f4aa79e5c1fa3c2ded70a6a7cac89d83be9735b
Daniel Anzola <danielanzola@usf.edu>
2021-03-26 14:11:03 -0400

    Created a table

--- myScript.sql
+++ myScript.sql
@@ -7,3 +7,16 @@ REPLACE INTO `appiandb`.`GID_REF_reference` (`refId`, `refType`, `refValue`, `is
 REPLACE INTO `appiandb`.`GID_REF_reference` (`refId`, `refType`, `refValue`, `isActive`, `createdOn`, `createdBy`) VALUES ('107', 'affiliation', 'ROTC', '1', UTC_TIMESTAMP(), 'U69227703');
 REPLACE INTO `appiandb`.`GID_REF_reference` (`refId`, `refType`, `refValue`, `isActive`, `createdOn`, `createdBy`) VALUES ('108', 'affiliation', 'INTO', '1', UTC_TIMESTAMP(), 'U69227703');
 REPLACE INTO `appiandb`.`GID_REF_reference` (`refId`, `refType`, `refValue`, `isActive`, `createdOn`, `createdBy`) VALUES ('109', 'affiliation', 'Research-Related', '1', UTC_TIMESTAMP(), 'U69227703');
+
+
+-- Create Relationship Mapping Audit History table
+
+CREATE TABLE IF NOT EXISTS `appiandb`.`GID_relationshipMappingAuditHistory` (
+  `relationshipMappingAuditHistoryId` INT(11) NOT NULL AUTO_INCREMENT,
+  `affiliation` INT(11) NULL,
+  `actionPerformed` INT(11) NULL,
+  `performedOn` DATETIME NULL,
+  `performedBy` VARCHAR(9) NULL,
+  `isVisible` TINYINT(1) NULL,
+  `changeDesc` VARCHAR(255) NULL,
+  PRIMARY KEY (`relationshipMappingAuditHistoryId`));
